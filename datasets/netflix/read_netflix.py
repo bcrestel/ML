4ae9ac63a88f66a38fa289b83e0db65111d5ad06
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2018-02-13 23:41:08 -0600

    construct sparse movie matrix

--- datasets/netflix/read_netflix.py
+++ datasets/netflix/read_netflix.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
-
+from scipy.sparse import csr_matrix
+import itertools
 
 def read_netflix(filename):
+    print('Read data')
     df = pd.read_csv(filename, header=None, names=['custID', 'rating', 'date'])
     # define a column with movieID
     df['movieID'] = df['custID']
@@ -10,7 +12,7 @@ def read_netflix(filename):
     df['movieID'].fillna(method='ffill',inplace=True)
     df['movieID'] = df['movieID'].str[:-1]
     # remove unused rows with movieID
-    df.drop(labels=(df['rating'] == np.NaN), axis=0, inplace=True)
+    df.drop(df[df.custID.str[-1]==':'].index, inplace=True)
 
     return df
 
@@ -22,8 +24,26 @@ def totaldistr(df):
 def distrratingpermovie(df):
     return (100.0*df.groupby(['movieID','rating']).count()/df.groupby(['movieID','rating']).count().groupby('movieID').sum())['custID']
 
+def moviematrix(df):
+    """ construct matrix of movie ratings
+    rows=users, cols=movies"""
+    print('Compute characteristics for movie matrix')
+    mmcomp = df.drop('date', axis=1)\
+                .groupby('custID')\
+                .agg({'movieID':lambda x:x.tolist(),\
+                      'rating':lambda x:x.tolist()})
+    data = np.array([int(x) for x in itertools.chain.from_iterable(mmcomp.rating.values)], dtype=np.float)
+    cols = np.array([int(x) for x in itertools.chain.from_iterable(mmcomp.movieID.values)], dtype=np.int)-1
+    rr = mmcomp.reset_index().apply(lambda x: [x.name for ii in x.movieID], axis=1)
+    rows = np.array(list(itertools.chain.from_iterable(rr)), dtype=np.int)
+    return data, rows, cols
+
+    
+    
 
 if __name__ == "__main__":
     df = read_netflix('combined_data_1_short.txt')
     td = totaldistr(df)
     tdpm = distrratingpermovie(df)
+    data, rows, cols = moviematrix(df)
+    spmat = csr_matrix((data, (rows, cols)))
