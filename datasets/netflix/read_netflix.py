af162383231d5b6b1cef9d53f643c53c5887cf54
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2018-02-16 00:22:10 -0600

    normalize ratings

--- datasets/netflix/read_netflix.py
+++ datasets/netflix/read_netflix.py
@@ -24,20 +24,35 @@ def totaldistr(df):
 def distrratingpermovie(df):
     return (100.0*df.groupby(['movieID','rating']).count()/df.groupby(['movieID','rating']).count().groupby('movieID').sum())['custID']
 
-def moviematrix(df):
+def groupbycustID(df):
+    return df.drop('date', axis=1)\
+            .groupby('custID')\
+            .agg({'movieID':lambda x:x.tolist(),\
+                  'rating':lambda x:x.tolist()})
+    
+def moviematrix(mmcomp):
     """ construct matrix of movie ratings
     rows=users, cols=movies"""
     print('Compute characteristics for movie matrix')
-    mmcomp = df.drop('date', axis=1)\
-                .groupby('custID')\
-                .agg({'movieID':lambda x:x.tolist(),\
-                      'rating':lambda x:x.tolist()})
-    data = np.array([int(x) for x in itertools.chain.from_iterable(mmcomp.rating.values)], dtype=np.float)
+    data = np.array([float(x) for x in itertools.chain.from_iterable(mmcomp.rating.values)], dtype=np.float)
     cols = np.array([int(x) for x in itertools.chain.from_iterable(mmcomp.movieID.values)], dtype=np.int)-1
     rr = mmcomp.reset_index().apply(lambda x: [x.name for ii in x.movieID], axis=1)
     rows = np.array(list(itertools.chain.from_iterable(rr)), dtype=np.int)
     return data, rows, cols
 
+
+def normalizedf(df):
+    print('Normalize ratings per customer')
+    dfmean = df.apply(lambda x : np.array(x.rating).mean(), axis=1)
+    dfstd = df.apply(lambda x : np.array(x.rating).std(), axis=1)
+    normalized = df.rating.apply(lambda x: 
+        [(ii-np.array(x).mean())/np.array(x).std() if np.array(x).std() > 0 
+         else 0.0 for ii in x])
+    return dfmean, dfstd, normalized
+
+def removesingleratings(df):
+    print('remove customers with a single rating')
+    return df[df.apply(lambda x : len(x.movieID), axis=1) > 1]
     
     
 
@@ -45,6 +60,14 @@ if __name__ == "__main__":
     df = read_netflix('combined_data_1_short.txt')
     td = totaldistr(df)
     tdpm = distrratingpermovie(df)
-    data, rows, cols = moviematrix(df)
+    
+    mmcomp = groupbycustID(df)
+    mmcomp = removesingleratings(mmcomp)
+    means, stds, normalizedratings = normalizedf(mmcomp)
+    mmcomp.rating = normalizedratings
+    
+    data, rows, cols = moviematrix(mmcomp)
+    print('Assemble sparse dataframe')
     spmat = coo_matrix((data, (rows, cols)))
     dfm = pd.SparseDataFrame(spmat)
+    
