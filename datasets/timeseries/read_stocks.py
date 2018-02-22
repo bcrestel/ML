d0793a2a302c42942e7d8f5ba538fe503056851b
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2018-02-21 22:16:20 -0600

    fix bug

--- datasets/timeseries/read_stocks.py
+++ datasets/timeseries/read_stocks.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
-def loadfile():
-    df = pd.read_csv('stocksIBMandCo.txt', header=None, skiprows=1, 
+def loadfile(filenameIBM):
+    df = pd.read_csv(filenameIBM, header=None, skiprows=1, 
                      delim_whitespace=True,
         names = ['date', 'MOBIL', 'IBM', 'WEYER', 'CITCRP', 'MARKET', 'RKFREE'])
-    df.date = df.date.astype(str)
-    df.date = pd.to_datetime(df.date.str.split('.').str.join('-'))
+    df.date = pd.to_datetime(
+            df.date.apply(lambda x: str(int(x)) + '-' + str(int(100*(x-int(x))+1))))
     return df
 
 def compounded(df):
+    """ convert simple returns into compounded returns """
     return (1.0 + df).cumprod() - 1.0
 
 if __name__ == "__main__":
-    df = loadfile()
+    df = loadfile('stocksIBMandCo.txt')
     df['MARKET_cpd'] = compounded(df.MARKET)
     df.MARKET_cpd.plot()    
